eb4565ecec014c89caadd3fc2f6079f027eb012d
naveen <naveen@LAPTOP-MGA5IJ3I>
2023-10-03 12:59:07 +0530

    CRUDSQL

---
+++ codesimple/src/test/java/com/codesimple/codesimple/CodesimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesimple.codesimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodesimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
